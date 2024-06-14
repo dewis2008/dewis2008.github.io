0cc1228ef201832191ee24994ed27515c77a3a5f
dewis2008 <deiwisp@gmail.com>
2024-06-14 12:45:56 +0300

    Portfolio Website
    Portfolio Website

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Sticky NavBar //
 
-const nav = document.querySelector(".NavBar");
-window.addEventListener("scroll", function () {
-  if (document.documentElement.scrollTop > 20) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-});
+// const nav = document.querySelector(".NavBar");
+// window.addEventListener("scroll", function () {
+//   if (document.documentElement.scrollTop > 20) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
 
 // TEXT TYPING EFFECT //
 
@@ -70,7 +70,7 @@ const JSCarousel = ({
   carouselSelector,
   slideSelector,
   enablePagination = true,
-  enableAutoplay = true,
+  enableAutoplay = false,
   autoplayInterval = 2000,
 }) => {
   // Find the carousel element in the DOM.
@@ -405,3 +405,26 @@ document.querySelectorAll(".main-nav-link").forEach((link) => {
     }
   });
 });
+
+/////////// Scroll to top //////////
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
